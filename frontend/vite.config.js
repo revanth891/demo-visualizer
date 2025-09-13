82fd1be79dd3847327347033287af4bd4952b8d1
RevanthK <52790923+revanth891@users.noreply.github.com>
2025-09-14 01:05:59 +0530

    publish

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        // target: 'http://0.0.0.0:3000',
+        target: 'https://demo-visualizer.onrender.com',
         changeOrigin: true,
         secure: false,
       }
